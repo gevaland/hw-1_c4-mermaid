e18190a4ad2ce17c1bd5937aeb5c7ca9661a601c
Nikita Matveev <matnik2001@gmail.com>
2024-02-18 20:19:41 +0200

    appended svg

--- src/usecases.py
+++ src/usecases.py
@@ -1,18 +1,17 @@
 from diagrams import Diagram, Cluster, Edge
-from diagrams.aws.general import User, Users, GenericDatabase, TraditionalServer
+from diagrams.aws.general import User, Users, TraditionalServer
 
 with Diagram("Use Case Diagram", show=True):
 
     # Define actors
     business_customer = User("Бизнес-заказчик")
     ml_team = Users("МЛ команда")
-    data_storage = GenericDatabase("Хранилище данных")
-    analytical_layer = TraditionalServer("Аналитический слой")
+    ml_service = TraditionalServer("Сервис ML Моделей")
+    online_cinema = TraditionalServer("Онлайн-кинотеатр")
 
     # Define relations
-    with Cluster("Отношения"):
+    with Cluster("Use Cases"):
         business_customer >> Edge(color="black", style="dashed", label="Направляет гипотезы") >> ml_team
-        business_customer >> Edge(color="black", style="dashed", label="Заказчик обращается к аналитическому слою для оценки эффекта") >> analytical_layer
-        ml_team >> Edge(color="black", style="dashed", label="Получение данных из хранилища") >> data_storage
-        ml_team >> Edge(color="black", style="dashed", label="Разработка решений в аналитический слой") >> analytical_layer
-        analytical_layer >> Edge(color="black", style="dashed", label="Получение данных из хранилища") >> data_storage
+        business_customer >> Edge(color="black", style="dashed", label="Заказчик обращается к онлайн-кинотеатру для оценки эффекта") >> online_cinema
+        ml_team >> Edge(color="black", style="dashed", label="Разработка и обучение ML моделей") >> ml_service
+        ml_service >> Edge(color="black", style="dashed", label="Развертывание ML моделей в онлайн-кинотеатр") >> online_cinema
